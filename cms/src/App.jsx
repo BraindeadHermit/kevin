533e30c3aff4d10ee334f62b47d98c775742d51e
BraindeadHermit <scorziello.giovanni00@gmail.com>
2023-10-29 18:28:36 +0100

    feat: folder structure, tailwind base colors

--- cms/src/App.jsx
+++ cms/src/App.jsx
@@ -1,5 +1,5 @@
 const App = () => {
-  return <div></div>;
+  return <div className="w-full h-screen bg-primary-variant"></div>;
 };
 
 export default App;
--- cms/tailwind.config.js
+++ cms/tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./src/**/*.{jsx,js,html}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          main: '#009688',
+          variant: '#00695F',
+        },
+        secondary: {
+          main: '#D500F9',
+          variant: '#9500AE',
+        },
+      },
+      fontFamily: {
+        body: 'Inter',
+      }
+    }
   },
   plugins: [],
 };
