d27af873cf4c5bcc2a1f57829202d082167eae38
Giovanni Scorziello <scorziello.giovanni00@gmail.com>
2023-11-30 11:05:46 +0100

    feat: loading logic start

--- game/pages/login/login.gd
+++ game/pages/login/login.gd
@@ -1,21 +1,25 @@
 extends Control
 
-var username_line_edit = null
-var company_code_line_edit = null
-var error_label = null
-var error_label_username = null
+@onready var username_line_edit = get_node("VBoxContainer2/VBoxContainer/UsernameLineEdit")
+@onready var company_code_line_edit = get_node("VBoxContainer2/VBoxContainer2/CompanyCodeLineEdit")
+@onready var error_label = get_node("VBoxContainer2/VBoxContainer2/error_label")
+@onready var error_label_username = get_node("VBoxContainer2/VBoxContainer/error_label_username")
+@onready var spinner = get_node("VBoxContainer/spinner")
+@onready var spinner_text = get_node("VBoxContainer/spinner_text")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	username_line_edit = get_node("VBoxContainer2/VBoxContainer/UsernameLineEdit")
-	company_code_line_edit = get_node("VBoxContainer2/VBoxContainer2/CompanyCodeLineEdit")
-	error_label = get_node("VBoxContainer2/VBoxContainer2/error_label")
-	error_label_username = get_node("VBoxContainer2/VBoxContainer/error_label_username")
-	
 	error_label.visible = false
 	error_label_username.visible = false
 
+func login(code):
+	spinner.visible = true
+	spinner_text.visible = true
+	if code == 'ciao':
+		return true
+	else: 
+		return false
 
 func _on_quit_btn_pressed():
 	get_tree().quit()
@@ -32,8 +36,9 @@ func _on_start_btn_pressed():
 	"""Username control block"""
 	if username != '':
 		"""Company code control block"""
-		if company_code == 'ciao':
-			get_tree().change_scene_to_file("res://pages/main-menu/main-menu.tscn")
+		if login(company_code):
+			spinner_text.text = "Caricamento..."
+			"""get_tree().change_scene_to_file("res://pages/main-menu/main-menu.tscn")"""
 		elif company_code == '':
 			error_label.text = "il codice della compagnia è obbligatorio"
 			error_label.visible = true
