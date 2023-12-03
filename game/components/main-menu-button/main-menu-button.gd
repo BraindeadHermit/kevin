28c279f500b1ce477ae9e5c0b8a331f412e0f4ea
Giovanni Scorziello <38634298+BraindeadHermit@users.noreply.github.com>
2023-12-03 17:15:39 +0100

    [game] main menu
    * feat: background animation
    
    * feat: set quit action
    
    * feat: spinner component
    
    * feat: loading logic start
    
    * feat: load the main scene on another trhead
    
    * feat: add main backgroud music
    
    * feat: load main menu images on another thread
    
    * feat: complete main-menu

---
+++ game/components/main-menu-button/main-menu-button.gd
@@ -0,0 +1,15 @@
+extends TextureButton
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var label = get_node("HBoxContainer/text")
+	label.text = get_meta("button_text")
+	var root = get_node("HBoxContainer/root")
+	root.text = Global.get_username()
+	
+
+func _on_root_resized():
+	var x = $HBoxContainer/text.size.x + $HBoxContainer/root.size.x + $HBoxContainer/tilde.size.x + $HBoxContainer/dollar.size.x + 10
+	self.set_size(Vector2(x, self.size.y))
+	self.texture_normal.get_image().resize(x, self.size.y, Image.INTERPOLATE_BILINEAR)
+	print(self.size)
---
+++ game/pages/main-menu/main-menu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+var images_url = ["res://assets/img/city.png", "res://assets/img/city2.png", "res://assets/img/hack.png", "res://assets/img/jet.png", "res://assets/img/motor.png", "res://assets/img/temple.png"]
+var images = []
+@onready var texture_rect = get_node("TextureRect")
+@onready var description = get_node("description")
+var thread = Thread.new()
+
+func _on_texture_rect_ready():
+	thread.start(_load_image)
+	images = thread.wait_to_finish()
+	
+	if not thread.is_alive():
+		var rng = RandomNumberGenerator.new()
+		while true:
+			var rand = rng.randi_range(0, 5)
+			$TextureRect.texture = images[rand]
+			await get_tree().create_timer(13).timeout
+			$TextureRect.texture = null
+			await get_tree().create_timer(1).timeout
+
+func _load_image():
+	var img = [load(images_url[0]), load(images_url[1]), load(images_url[2]), load(images_url[3]), load(images_url[4]), load(images_url[5])]
+	return img
+
+func _on_quit_pressed():
+	get_tree().quit()
