d149e1e832658cfab71c04fee6573c6345a18b14
mariopeluso <mariopeluso00@gmail.com>
2023-12-15 10:02:37 +0100

    feat: install react router and define routing pages

--- cms/src/App.jsx
+++ cms/src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import LogIn from "./pages/login/LogIn";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 const App = () => {
   return (
     <div>
-      <LogIn />
+      <Routes>
+        <Route path="/" element={<LogIn />}/>
+        <Route path="/home" element={<Dashboard/> } />
+      </Routes>
     </div>
   );
 };
