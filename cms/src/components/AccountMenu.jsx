39c8c88f17116bbe8eb4027a41b6fbbc074de9e5
mariopeluso <mariopeluso00@gmail.com>
2023-12-22 16:05:01 +0100

    fix: import unused and deprecated props

--- cms/src/components/AccountMenu.jsx
+++ cms/src/components/AccountMenu.jsx
@@ -6,9 +6,7 @@ import MenuItem from '@mui/material/MenuItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import Tooltip from '@mui/material/Tooltip';
-import PersonAdd from '@mui/icons-material/PersonAdd';
 import Settings from '@mui/icons-material/Settings';
 import Logout from '@mui/icons-material/Logout';
 
@@ -43,7 +41,7 @@ export default function AccountMenu() {
         open={open}
         onClose={handleClose}
         onClick={handleClose}
-        PaperProps={{
+        slotProps={{
           elevation: 0,
           sx: {
             overflow: 'visible',
