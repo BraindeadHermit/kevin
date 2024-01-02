f09cae0ad193f9182ec9abcda585c6cab4f6438a
Giovanni Scorziello <38634298+BraindeadHermit@users.noreply.github.com>
2024-01-02 20:25:51 +0100

    [game] level 1 graphics (#71)
    * feat: base game layout
    
    * feat: add level visual details
    
    * feat: background finish
    
    * refactor: image import
    
    * feat: graphic finish

--- game/pages/main-menu/main-menu.gd
+++ game/pages/main-menu/main-menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var images_url = ["res://assets/img/city.png", "res://assets/img/city2.png", "res://assets/img/hack.png", "res://assets/img/jet.png", "res://assets/img/motor.png", "res://assets/img/temple.png"]
+var images_url = ["res://assets/img/city.png", "res://assets/img/hack.png", "res://assets/img/jet.png", "res://assets/img/motor.png", "res://assets/img/temple.png", "res://assets/img/cyber1.jpg", "res://assets/img/cyber2.jpg", "res://assets/img/cyber3.jpg"]
 var images = []
 @onready var texture_rect = get_node("TextureRect")
 @onready var description = get_node("description")
@@ -13,15 +13,19 @@ func _on_texture_rect_ready():
 	if not thread.is_alive():
 		var rng = RandomNumberGenerator.new()
 		while true:
-			var rand = rng.randi_range(0, 5)
+			var rand = rng.randi_range(0, 7)
 			$TextureRect.texture = images[rand]
 			await get_tree().create_timer(13).timeout
 			$TextureRect.texture = null
 			await get_tree().create_timer(1).timeout
 
 func _load_image():
-	var img = [load(images_url[0]), load(images_url[1]), load(images_url[2]), load(images_url[3]), load(images_url[4]), load(images_url[5])]
+	var img = [load(images_url[0]), load(images_url[1]), load(images_url[2]), load(images_url[3]), load(images_url[4]), load(images_url[5]), load(images_url[6]), load(images_url[7])]
 	return img
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_play_pressed():
+	get_tree().call_deferred("change_scene_to_file", "res://game/level_1/level_1.tscn")
