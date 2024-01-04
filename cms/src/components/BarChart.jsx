492b1f54e46d0d88c445ad8d9a73fabb2bf44716
Mario Peluso <81226923+MarioPeluso@users.noreply.github.com>
2024-01-04 22:25:27 +0100

    feat: complete UI regarding BarChart (#72)

---
+++ cms/src/components/BarChart.jsx
@@ -0,0 +1,60 @@
+import { Bar } from 'react-chartjs-2'
+import { Chart as ChartJS } from 'chart.js/auto'
+
+export default function BarChart() {
+    return <Bar data={{
+        labels: ['livello 1', 'livello 2'],
+        datasets: [{
+            maxBarThickness: 50,
+            borderRadius: 8,
+            axis: 'y',
+            label: 'Risposte esatte',
+            data: [65, 59],
+            fill: true, 
+            backgroundColor: ['green'],
+            borderWidth: 0,
+        },
+        {
+            maxBarThickness: 50,
+            borderRadius: 8,
+            axis: 'y',
+            label: 'Risposte errate',
+            data: [65, 59],
+            fill: true, 
+            backgroundColor: ['red'],
+            borderWidth: 0,
+        },
+    ]
+    }} 
+    options={{
+        responsive: true,
+        scales: {
+            y: {
+                ticks: {
+                    font: {
+                        size: 18,
+                        weight: '500'
+                    }
+                }
+            },
+            x: {
+                ticks: {
+                    font: {
+                        size: 16
+                    }
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                labels: {
+                    font: {
+                        size: 16
+                    }
+                }
+            }
+        },
+        indexAxis: 'y',
+        }}/>
+} 
+  
