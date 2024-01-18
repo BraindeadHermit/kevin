4357c69b729a6060da27e8946ebd854f0121c4de
mariopeluso <mariopeluso00@gmail.com>
2024-01-18 14:36:33 +0100

    fix: add answers in question card

--- cms/src/components/QuestionCard.jsx
+++ cms/src/components/QuestionCard.jsx
@@ -1,19 +1,13 @@
-import { Box, Button, Divider, LinearProgress, Typography, styled } from "@mui/material";
+import { Box, Button, Divider, LinearProgress, Tooltip, Typography, styled } from "@mui/material";
 
 const Progress = styled(LinearProgress)(({ theme }) => ({
   height: 20,
   borderRadius: 12,
-  [`&.${LinearProgress.colorPrimary}`]: {
-    backgroundColor: 'green',
-  },
-  [`& .${LinearProgress.bar}`]: {
-    backgroundColor: theme.palette.error.main,
-  },
 }));
 
 export default function QuestionCard() {
   return (
-  <Box border={0.2} borderColor={'#616161'} sx={{padding: 1, borderRadius: 8, width: 'auto'}}>
+  <Box border={0.2} borderColor={'#616161'} sx={{padding: 1.2, borderRadius: 8, width: 'auto'}}>
     <Box display="flex" justifyContent="space-between" alignItems="start" sx={{padding: 1.5}}>
       <Box sx={{display: 'flex', flexDirection: 'column', maxWidth: '70%'}}>
         <Typography variant="subtitle1" fontWeight={800}>Quale tipo di protocollo viene utilizzato per effettuare una richiesta al server?</Typography>
@@ -22,15 +16,49 @@ export default function QuestionCard() {
       <Button variant="contained" disableElevation>Edit Question</Button>
     </Box>
     <Divider variant="middle" sx={{borderWidth: 1, margin: 1}}></Divider>
-    <Box sx={{display: 'flex', flexDirection: 'column', padding: 1}}>
+    <Box sx={{display: 'flex', flexDirection: 'column'}}>
       <Progress variant="determinate" value={35}></Progress>
-      <Typography variant="subtitle2" sx={{paddingTop: 1, paddingBottom: 1}} color={'#616161'}>risposte totali: 2</Typography>
-      <Progress variant="determinate" value={20}></Progress>
-      <Typography variant="subtitle2" sx={{paddingTop: 1, paddingBottom: 1}} color={'#616161'}>risposte totali: 10</Typography>
-      <Progress variant="determinate" value={30}></Progress>
-      <Typography variant="subtitle2" sx={{paddingTop: 1, paddingBottom: 1}} color={'#616161'}>risposte totali: 16</Typography>
-      <Progress variant="determinate" value={12}></Progress>
-      <Typography variant="subtitle2" sx={{paddingTop: 1, paddingBottom: 1}} color={'#616161'}>risposte totali: 8</Typography>
+      <Box sx={{display: 'flex', flexDirection: 'row', padding: 1, justifyContent: 'space-between'}}> 
+        <Box maxWidth={'40%'}>
+          <Tooltip title={'UDP'} placement="bottom-start">
+            <Typography noWrap={true} variant="subtitle2" fontWeight={900} color={'#616161'}>UDP</Typography>
+          </Tooltip>
+        </Box>
+        <Typography variant="subtitle2" color={'#616161'}>risposte totali: 2</Typography>
+      </Box>
+    </Box>
+    <Box sx={{display: 'flex', flexDirection: 'column'}}>
+      <Progress variant="determinate" value={35} sx={{color: (correttezza) => (correttezza === 'si' ? 'green' : 'red')}}></Progress>
+      <Box sx={{display: 'flex', flexDirection: 'row', padding: 1, justifyContent: 'space-between'}}> 
+        <Box maxWidth={'40%'}>
+          <Tooltip title={'UDP'} placement="bottom-start">
+            <Typography noWrap={true} variant="subtitle2" fontWeight={900} color={'#616161'}>UDP</Typography>
+          </Tooltip>
+        </Box>
+        <Typography variant="subtitle2" color={'#616161'}>risposte totali: 2</Typography>
+      </Box>
+    </Box>
+    <Box sx={{display: 'flex', flexDirection: 'column'}}>
+      <Progress variant="determinate" value={35}></Progress>
+      <Box sx={{display: 'flex', flexDirection: 'row', padding: 1, justifyContent: 'space-between'}}> 
+        <Box maxWidth={'40%'}>
+          <Tooltip title={'UDP'} placement="bottom-start">
+            <Typography noWrap={true} variant="subtitle2" fontWeight={900} color={'#616161'}>UDP</Typography>
+          </Tooltip>
+        </Box>
+        <Typography variant="subtitle2" color={'#616161'}>risposte totali: 2</Typography>
+      </Box>
+    </Box>
+    <Box sx={{display: 'flex', flexDirection: 'column'}}>
+      <Progress variant="determinate" value={35}></Progress>
+      <Box sx={{display: 'flex', flexDirection: 'row', padding: 1, justifyContent: 'space-between'}}> 
+        <Box maxWidth={'40%'}>
+          <Tooltip title={'UDP'} placement="bottom-start">
+            <Typography noWrap={true} variant="subtitle2" fontWeight={900} color={'#616161'}>UDP</Typography>
+          </Tooltip>
+        </Box>
+        <Typography variant="subtitle2" color={'#616161'}>risposte totali: 2</Typography>
+      </Box>
     </Box>
   </Box>
   )
