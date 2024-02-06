b372ed2a7091d82fa196245f0721865b885eccb8
Giovanni Scorziello <scorziello.giovanni00@gmail.com>
2024-02-06 17:26:37 +0100

    feat: level finished base component

--- game/game/level_1/level_1.gd
+++ game/game/level_1/level_1.gd
@@ -9,12 +9,13 @@ signal collectables_setup(collectables_data)
 
 var terminals
 var collectables
+var level_id
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Kevin.player_init()
 	
-	var level_id = await level_access.create_new_level(Global.get_current_match_id(), "livello 1", "malware")
+	level_id = await level_access.create_new_level(Global.get_current_match_id(), "livello 1", "malware")
 	
 	var answered = await level_access.get_answered_questions_number(level_id)
 	
@@ -27,7 +28,10 @@ func _ready():
 	collectables = await collectable_access.get_collectables_by_level_id(level_id)
 	
 	await collectables_setup.emit(collectables)
-	
-	
-	
-	
+
+func _on_area_2d_body_entered(body):
+	var ans_questions = await level_access.get_answered_questions_number(level_id)
+	if ans_questions == 5:
+		print("Level Passed")
+	else:
+		print("Non hai risposto a tutte le domande")
