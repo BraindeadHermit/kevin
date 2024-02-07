1e88738ce50e0e73fa5ffcbbd7824d8ccc76919c
BraindeadHermit <scorziello.giovanni00@gmail.com>
2024-02-07 12:40:18 +0100

    feat: player detection

--- game/game/slime_enemy/slime.gd
+++ game/game/slime_enemy/slime.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 25.0
+var SPEED = 25.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -22,10 +22,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	elif is_on_wall() or (not $floor_detector.is_colliding() and should_detect_floor):
-		direction = direction * -1
-		$AnimatedSprite2D.flip_h = not $AnimatedSprite2D.flip_h
-		$floor_detector.position.x = $CollisionShape2D.shape.radius * direction
-
+		turn(direction * -1, not $AnimatedSprite2D.flip_h)
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	velocity.x = direction * SPEED
@@ -35,3 +32,18 @@ func _physics_process(delta):
 
 func _on_player_detector_body_entered(body):
 	body.hurt(position.x)
+
+func _on_player_detected(body):
+	SPEED = 75.0
+	if body.position.x < position.x:
+		turn(-1, false)
+	elif body.position.x > position.x:
+		turn(1, true)
+
+func _on_player_not_detected(body):
+	SPEED = 25.0
+	
+func turn(new_direction, flip_sprite):
+	direction = new_direction
+	$AnimatedSprite2D.flip_h = flip_sprite
+	$floor_detector.position.x = $CollisionShape2D.shape.radius * direction
