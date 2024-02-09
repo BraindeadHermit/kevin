61599c4ab51eac2b2b2b1e79c4695c0e2402efb7
Giovanni Scorziello <38634298+BraindeadHermit@users.noreply.github.com>
2024-02-09 12:20:10 +0100

    refactor: main menu graphical rework (#92)

--- game/components/main-menu-button/main-menu-button.gd
+++ game/components/main-menu-button/main-menu-button.gd
@@ -2,16 +2,4 @@ extends TextureButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var label = get_node("HBoxContainer/text")
-	label.text = get_meta("button_text")
-	var root = get_node("HBoxContainer/root")
-	var username = Global.get_username()
-	if len(username) > 9:
-		username = username.substr(0, 9) + "..."
-	
-	root.text = username
-	
-
-func _on_root_resized():
-	var x = $HBoxContainer/text.size.x + $HBoxContainer/root.size.x + $HBoxContainer/tilde.size.x + $HBoxContainer/dollar.size.x + 10
-	self.set_size(Vector2(x, self.size.y))
+	$text.text = get_meta("button_text")
